2aa119de175288b4ee68d32322bf6ee2ea028901
Amos Feng <zfeng@redhat.com>
2016-04-08 16:46:45 +0800

    update to add the h2 database config

--- spring-boot/src/main/java/org/jboss/narayana/quickstarts/springboot/config/TransactionConfig.java
+++ spring-boot/src/main/java/org/jboss/narayana/quickstarts/springboot/config/TransactionConfig.java
@@ -1,14 +1,12 @@
 package org.jboss.narayana.quickstarts.springboot.config;
 
-import com.arjuna.ats.jta.TransactionManager;
+import com.arjuna.ats.internal.jta.transaction.arjunacore.TransactionManagerImple;
 import com.arjuna.ats.jta.UserTransaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.EnableLoadTimeWeaving;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -16,34 +14,45 @@ import org.springframework.transaction.annotation.TransactionManagementConfigure
 import org.springframework.transaction.jta.JtaTransactionManager;
 
 import javax.sql.DataSource;
+import javax.transaction.TransactionManager;
 
 /**
  * Created by jhaskovec on 4/4/16.
  */
 @Configuration
 @EnableTransactionManagement
-//@EnableLoadTimeWeaving(aspectjWeaving=EnableLoadTimeWeaving.AspectJWeaving.ENABLED)
 public class TransactionConfig implements TransactionManagementConfigurer {
-	private static final Logger log = LoggerFactory.getLogger(TransactionConfig.class);
+    private static final Logger log = LoggerFactory.getLogger(TransactionConfig.class);
 
-	@Autowired
-	private DataSource datasource;
+    @Autowired
+    private DataSource datasource;
 
-	@Override
-	public PlatformTransactionManager annotationDrivenTransactionManager() {
-		JtaTransactionManager transactionManager = new JtaTransactionManager(UserTransaction.userTransaction(), TransactionManager.transactionManager());
-		transactionManager.setTransactionManagerName("transactionManager");
+    @Bean
+    public PlatformTransactionManager jtaTransactionManager() {
+        JtaTransactionManager tm = new JtaTransactionManager();
+        tm.setUserTransaction(UserTransaction.userTransaction());
+        tm.setTransactionManager(transactionManager());
+        tm.setTransactionManagerName("transactionManager");
 
-		return transactionManager;
-	}
+        return tm;
+    }
 
-	@Bean
-	JdbcTemplate jdbcTemplate(DataSource dataSource) {
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-		log.info("Creating tables");
-		jdbcTemplate.execute("drop table example if exists");
-		jdbcTemplate.execute("CREATE TABLE example ( id INT(10), name varchar(32), PRIMARY KEY(id))");
-		return jdbcTemplate;
-	}
+    @Bean
+    public TransactionManager transactionManager() {
+        return new TransactionManagerImple();
+    }
 
+    @Bean
+    JdbcTemplate jdbcTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(datasource);
+        log.info("Creating tables");
+        jdbcTemplate.execute("drop table example if exists");
+        jdbcTemplate.execute("CREATE TABLE example ( id INT(10), name varchar(32), PRIMARY KEY(id))");
+        return jdbcTemplate;
+    }
+
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return jtaTransactionManager();
+    }
 }
